f207491c112c78eb6c5c7dbaad0d481203485107
faiz <razafaiz815@gmail.com>
2021-01-16 20:25:38 +0500

    Loader added, UI styled

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
-import React, { FC, lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { RootState } from '../rootReducer';
+import React, { FC, lazy, Suspense } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../rootReducer";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
-const Auth = lazy(() => import('../features/auth/Auth'));
-const Home = lazy(() => import('../features/home/Home'));
+const Auth = lazy(() => import("../features/auth/Auth"));
+const Home = lazy(() => import("../features/home/Home"));
 
 const App: FC = () => {
   const isLoggedIn = useSelector(
@@ -15,7 +17,18 @@ const App: FC = () => {
     <Router>
       <Switch>
         <Route path="/">
-          <Suspense fallback={<p>Loading...</p>}>
+          <Suspense
+            fallback={
+              <Loader
+                type="ThreeDots"
+                color="#00BFFF"
+                height={100}
+                width={100}
+                timeout={3000} //3 secs
+                className="Loader"
+              />
+            }
+          >
             {isLoggedIn ? <Home /> : <Auth />}
           </Suspense>
         </Route>

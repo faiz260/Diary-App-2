f10211cb55e8ff04433229ff7c97a174a937f340
faiz <razafaiz815@gmail.com>
2021-01-21 18:24:28 +0500

    App deployed

---
+++ build/precache-manifest.a3bb8971ba2ee09ace9c4a550e02556c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca7f77abcbcd79cd8cba1899caee5d50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a68021d914296b3c8f7",
+    "url": "/static/css/3.b1f234f1.chunk.css"
+  },
+  {
+    "revision": "03f90fe4fccb9db3ffd0",
+    "url": "/static/css/main.a9420d32.chunk.css"
+  },
+  {
+    "revision": "9391a2caa5439a943c41",
+    "url": "/static/js/0.e2f2b46a.chunk.js"
+  },
+  {
+    "revision": "6a68021d914296b3c8f7",
+    "url": "/static/js/3.f22614b8.chunk.js"
+  },
+  {
+    "revision": "5ff0d0562743023b04cddccce0f48115",
+    "url": "/static/js/3.f22614b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b627713905de1e0680d0",
+    "url": "/static/js/4.b5311733.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/4.b5311733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb98bebcb2f84e8a8c99",
+    "url": "/static/js/5.8405c702.chunk.js"
+  },
+  {
+    "revision": "3f225299079350724ff1",
+    "url": "/static/js/6.86622348.chunk.js"
+  },
+  {
+    "revision": "03f90fe4fccb9db3ffd0",
+    "url": "/static/js/main.a8567261.chunk.js"
+  },
+  {
+    "revision": "7e736909f3c8a0542658",
+    "url": "/static/js/runtime-main.5c3933ba.js"
+  }
+]);
